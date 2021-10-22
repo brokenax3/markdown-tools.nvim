756734459b5e7f5e816496ea3d6d0c83bceb1f0f
Mark Lee <mlcy0508@gmail.com>
2021-10-22 16:23:07 +1100

    Added MarkdownImageDelete.
    Checks if image directory exists before moving it.

--- lua/markdown-paste/init.lua
+++ lua/markdown-paste/init.lua
@@ -5,20 +5,24 @@ local function is_screenshot(filepath)
     if file~=nil then io.close(file) return true else return false end
 end
 
-local function send_to_cwd(filepath)
-    local currentDirectory = vim.fn.getcwd(0)
+local function send_to_cwd(filepath, currentDirectory)
     local ssDirectory = '/home/mark/Pictures/'
     local filename = filepath:gsub(ssDirectory, '')
-
-    -- Make new directory string
-    return currentDirectory .. '/' .. filename
+    
+    return currentDirectory .. filename
 end
 
 function M.MarkdownImagePaste()
+    local imgDirectory = vim.fn.getcwd(0) .. '/images/'
+    
+    if vim.fn.isdirectory(imgDirectory) == false then
+        print('Image directory does not exist. Creating images directory...')
+        vim.api.nvim_command('!mkdir ' .. imgDirectory)
+    end
 
     local file = vim.fn.getreg('*'):gsub('\n', '')
     if is_screenshot(file) then
-        local n_file = send_to_cwd(file)
+        local n_file = send_to_cwd(file, imgDirectory)
         local imagetext = '![](' .. n_file .. ')'
         vim.api.nvim_command('!mv ' .. file .. ' ' .. n_file)
 
@@ -26,6 +30,27 @@ function M.MarkdownImagePaste()
         print('Image link is pasted into the buffer')
     else
         print('Not a screenshot')
+    end
+
+end
+
+function M.MarkdownImageDelete()
+    -- Test if the string under the cursor matches '![]'
+    local imagetext = vim.fn.getline('.')
+
+    if imagetext:find('!%b[]') ~= nil then
+        local file = imagetext:match('!%[.*]%((.+)%)')
+        
+        if is_screenshot(file) then
+            if vim.fn.input('File ' .. file .. ' found. Delete? (y/n) ') == 'y' then
+                vim.api.nvim_command('!rm ' .. file)
+                vim.api.nvim_del_current_line()
+            else
+                print('File ' .. file .. ' not deleted.')
+            end
+        else
+            print('File not found.')
+        end
     end
 end
 
--- plugin/markdown-image-paste.lua
+++ plugin/markdown-image-paste.lua
@@ -1,3 +1,4 @@
 if vim.bo.filetype == 'pandoc' then
     vim.api.nvim_exec("command! MarkdownImagePaste lua require'markdown-paste'.MarkdownImagePaste()", true)
+    vim.api.nvim_exec("command! MarkdownImageDelete lua require'markdown-paste'.MarkdownImageDelete()", true)
 end
