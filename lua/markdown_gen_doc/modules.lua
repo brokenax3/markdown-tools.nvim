1ebd9f07a37f10ee78839da356b0391edbc594d9
Mark Lee <mlcy0508@gmail.com>
2021-12-17 12:12:41 +1100

    Fixed using the wrong path for file generation

--- lua/markdown_gen_doc/modules.lua
+++ lua/markdown_gen_doc/modules.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 local function check_paths()
-    local target_dir = '..' .. vim.fn.getcwd(0) .. 'docs'
+    local target_dir = '../docs'
 
     if vim.fn.isdirectory(target_dir) == 0 then
         print('Output directory does not exist. Creating directory at ' .. target_dir)
@@ -12,13 +12,13 @@ end
 
 function M.MarkdownGenPdf()
 
-    local filename_markdown = vim.fn.expand('%') .. '.md'
-    local filename_docs = vim.fn.expand('%') .. '.pdf'
+    local filename_markdown = vim.fn.expand('%')
+    local filename_docs = vim.fn.expand('%'):gsub('.md', '.pdf')
 
     -- Check if the output path exists.
     check_paths()
     
-    vim.api.nvim_command('!pandoc -V geometry:margin=1in ' .. filename_markdown .. ' -o ' .. filename_docs)
+    vim.api.nvim_command('!pandoc -V geometry:margin=1in ' .. filename_markdown .. ' -o ../docs/' .. filename_docs)
 
     print('Document Generated.')
 end
