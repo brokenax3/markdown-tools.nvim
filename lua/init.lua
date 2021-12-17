1c3534c393e2cca96d6f05614f2c9872523c70cb
Mark Lee <mlcy0508@gmail.com>
2021-12-17 12:21:30 +1100

    Removed init.lua
    Not completely sure what is best practice at this point.
    But this file is not required to run the plugin.

--- lua/init.lua
+++
@@ -1,5 +0,0 @@
--- require('markdown-paste.config')
--- markdown_image_paste = require('markdown_image_paste.modules')
-markdown_gen_doc = require('markdown_gen_doc.modules')
-
-return markdown_gen_doc
