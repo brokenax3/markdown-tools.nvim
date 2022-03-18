d453dd43b6e59667e3d1c98ff65c3764f8c7fd72
Mark Lee <mlcy0508@gmail.com>
2022-03-19 01:20:54 +1100

    Call functions regardless of filetype

--- plugin/markdown_tools.lua
+++ plugin/markdown_tools.lua
@@ -2,13 +2,10 @@ if vim.g.loaded_markdown_tools == 1 then
     return
 end
 
-if vim.bo.filetype == "pandoc" or "markdown" then
-    vim.api.nvim_exec("command! MarkdownImagePaste lua require'markdown_image_paste.modules'.MarkdownImagePaste()", true)
-    vim.api.nvim_exec("command! MarkdownImageDelete lua require'markdown_image_paste.modules'.MarkdownImageDelete()", true)
-    vim.api.nvim_exec("command! MarkdownGenPdf lua require'markdown_gen_doc.modules'.MarkdownGenPdf()", true)
-elseif vim.bo.filetype == "tex" then
-    vim.api.nvim_exec("command! TexImagePaste lua require'markdown_image_paste.modules'.TexImagePaste()", true)
-    vim.api.nvim_exec("command! TexImageDelete lua require'markdown_image_paste.modules'.TexImageDelete()", true)
-end
+vim.api.nvim_exec("command! MarkdownImagePaste lua require'markdown_image_paste.modules'.MarkdownImagePaste()", true)
+vim.api.nvim_exec("command! MarkdownImageDelete lua require'markdown_image_paste.modules'.MarkdownImageDelete()", true)
+vim.api.nvim_exec("command! MarkdownGenPdf lua require'markdown_gen_doc.modules'.MarkdownGenPdf()", true)
+vim.api.nvim_exec("command! TexImagePaste lua require'markdown_image_paste.modules'.TexImagePaste()", true)
+vim.api.nvim_exec("command! TexImageDelete lua require'markdown_image_paste.modules'.TexImageDelete()", true)
 
 vim.g.loaded_markdown_tools = 1
